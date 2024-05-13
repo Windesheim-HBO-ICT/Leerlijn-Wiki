6af210c7cff656cb8cf6a74425eda601904573d2
jaelkwakkel <62651445+jaelkwakkel@users.noreply.github.com>
2024-05-13 20:50:53 +0200

    Update readme

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Windesheim leertaken",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "localhost",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
