d921134e1f04d9369f2bc1ac0777a0b783a55370
Martijn Schuman <martijnschuman@hotmail.com>
2024-05-25 18:29:31 +0200

    Updated sidebar

--- quartz/quartz/components/ExplorerNode.tsx
+++ quartz/quartz/components/ExplorerNode.tsx
@@ -225,7 +225,7 @@ export function ExplorerNode({ node, opts, fullPath, fileData }: ExplorerNodePro
             <ul
               // Inline style for left folder paddings
               style={{
-                paddingLeft: node.name !== "" ? "1.4rem" : "0",
+                paddingLeft: node.name !== "" ? "0.75rem" : "0",
               }}
               class="content"
               data-folderul={folderPath}
