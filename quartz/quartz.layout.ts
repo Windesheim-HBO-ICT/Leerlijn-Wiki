7b307b507c74e09fb55fd5f8a7847dd7b5d97454
Martijn Schuman <martijnschuman@hotmail.com>
2024-05-28 19:36:26 +0200

    Updated styling & removed unnecessary files
    Removed default Quartz styling and replaced it for custom grid which works way better. Also removed unnecessary files

--- quartz/quartz.layout.ts
+++ quartz/quartz.layout.ts
@@ -33,8 +33,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    // Component.Backlinks(),
+    Component.TableOfContents(),
   ],
 }
 
