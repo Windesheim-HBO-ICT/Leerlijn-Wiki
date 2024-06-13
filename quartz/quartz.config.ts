e8b018b0a6e0dbed37116767bea2161bf429f6f0
Martijn Schuman <martijnschuman@hotmail.com>
2024-06-13 22:33:28 +0200

    External links open in new tab

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -69,7 +69,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest", openLinksInNewTab: true }),
       Plugin.Description(),
       Plugin.CodeRunner(),
     ],
