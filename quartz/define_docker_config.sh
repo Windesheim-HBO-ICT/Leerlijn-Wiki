74db64c61816788918d8aa737cab48b61f2bce75
Eltjo Voorhoeve <ej.voorhoeve@windesheim.nl>
2025-03-14 16:41:54 +0100

    - Upgraded clone_repo.sh, so it is more flexible with other repos. - Added define_docker_config.sh, to define vars about which repo to build and run - Added reset_docker_leertaken.sh, it builds and runs a repo and resets the docker container - Updated README

---
+++ quartz/define_docker_config.sh
@@ -0,0 +1,9 @@
+REPO_URL="https://github.com/Windesheim-HBO-ICT/Leerlijn-Content-SE"
+
+BRANCH_NAME="main"
+BRANCH_CONTENT_FOLDER="build"
+# BRANCH_NAME="feature/talstelsels"
+# BRANCH_CONTENT_FOLDER="content"
+
+IMAGE_NAME=windesheim-leertaken-quartz
+CONTAINER_NAME=mijn_leertaken
